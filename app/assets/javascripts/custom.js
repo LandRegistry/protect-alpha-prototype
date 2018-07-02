73392a996025db290bd42c6dae179ef7bcf806a9
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2018-07-02 09:45:10 +0100

    Happy path added

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,13 +1,11 @@
 $('.govuk-button[value="Find address"]').bind('click', function() {
-    var data = $('.govuk-input').val();
-    $('.govuk-custom-element-to-hide').toggle();
-    $('.govuk-custom-element-to-display').toggle();
-    $('.govuk-custom-data-to-display').text(data);
+    let $data = $('.govuk-input').val();
+    $('.govuk-custom-element-to-hide, .govuk-custom-element-to-display').toggle();
+    $('.govuk-custom-data-to-display').text($data);
     return false;
 });
 
 $('.govuk-custom-revert-state').bind('click', function() {
-    $('.govuk-custom-element-to-hide').toggle();
-    $('.govuk-custom-element-to-display').toggle();
+    $('.govuk-custom-element-to-hide, .govuk-custom-element-to-display').toggle();
     return false;
-})
+});
--- app/routes.js
+++ app/routes.js
@@ -8,4 +8,18 @@ router.get('/', function (req, res) {
 
 // Add your routes here - above the module.exports line
 
+// Email vs. address (e.g. v0.1 /add-new-contact)
+
+// Branching
+router.get('/v0-1/address-change', function (req, res) {
+  var toChange = req.query.addContact
+
+  if (toChange === 'email') {
+    // Redirect to the relevant page
+    res.redirect('confirm-email-addition')
+  } else {
+    res.render('v0-1/address-change')
+  }
+})
+
 module.exports = router
