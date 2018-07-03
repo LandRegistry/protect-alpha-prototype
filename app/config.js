b8ded8285cdf1ecc887a0aaa17f0894a92fc5d99
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2018-07-03 16:46:37 +0100

    Content update

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Protect my property',
+  serviceName: 'Change my address',
 
   // Default port that prototype runs on
   port: '3000',
