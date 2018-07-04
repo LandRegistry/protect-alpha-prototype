d41b38cd68936f28b04584baf9805d78e8479aa8
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2018-07-04 12:09:02 +0100

    Verify added; page layout updates

--- app/routes.js
+++ app/routes.js
@@ -19,5 +19,16 @@ router.get('/v0-1/add-address', function (req, res) {
     res.render('v0-1/add-address')
   }
 })
+// Verify route
+router.get('/v0-1/verification-intro', function (req, res) {
+  var toAdd = req.query.signIn
+
+  if (toAdd === "I've used Verify before") {
+    // Redirect to the relevant page
+    res.redirect('verification-choose')
+  } else {
+    res.render('v0-1/verification-intro')
+  }
+})
 
 module.exports = router
