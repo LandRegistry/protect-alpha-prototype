e8d2b0e4f4b56b9516891600ede303d3ab7f1009
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2018-07-20 12:48:34 +0100

    See index file for v0.3 changes

--- app/routes.js
+++ app/routes.js
@@ -19,7 +19,7 @@ router.get('/*/add-address', function (req, res) {
     res.redirect('confirm-phone-addition')
   }   
   else {
-    res.render('v0-2-1/add-address')
+    res.render('v0-3/add-address')
   }
 })
 
@@ -30,7 +30,7 @@ router.get('/*/change-contact', function (req, res) {
     // Redirect to the relevant page
     res.redirect('add-new-contact')
   } else {
-    res.render('v0-2-1/change-contact')
+    res.render('v0-3/change-contact')
   }
 })
 
@@ -41,7 +41,7 @@ router.get('/*/change-contact-2', function (req, res) {
     // Redirect to the relevant page
     res.redirect('change-second-contact-2')
   } else {
-    res.render('v0-2-1/change-contact-2')
+    res.render('v0-3/change-contact-2')
   }
 })
 
@@ -55,7 +55,7 @@ router.get('/*/update-contact-2-what', function (req, res) {
     res.redirect('delete-contact-2')
   }  
   else {
-    res.render('v0-2-1/update-contact-2-what')
+    res.render('v0-3/update-contact-2-what')
   }
 })
 
@@ -67,7 +67,7 @@ router.get('/*/verification-intro', function (req, res) {
     // Redirect to the relevant page
     res.redirect('verification-choose')
   } else {
-    res.render('v0-2-1/verification-intro')
+    res.render('v0-3/verification-intro')
   }
 })
 
@@ -78,18 +78,19 @@ router.get('/*/update-details-multi-exists', function (req, res) {
   if (confirmAddress === 'yes') {
     res.redirect('address-is-correct')
   } else {
-    res.render('v0-2-1/update-details-multi-exists')
+    res.render('v0-3/update-details-multi-exists')
   }
 })
 
-// Title numbers (e.g. v0-2/title-number)
+// Title numbers (e.g. v0-3/title-number)
 router.get('/*/title-number-entry', function (req, res) {
   var titleExists = req.query.title
 
   if (titleExists === 'no') {
     res.redirect('address-lookup')
   } else {
-    res.render('v0-2-1/title-number-entry')
+    res.render('v0-3/title-number-entry')
   }
 })
+
 module.exports = router
